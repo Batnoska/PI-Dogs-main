98ad7a397522a2e5b4ecc6db232439f61271d573
batnoska <batnoskasantiago@gmail.com>
2022-10-07 13:10:16 -0300

    Testing y Production

--- api/tests/models/dog.spec.js
+++ api/tests/models/dog.spec.js
@@ -9,13 +9,24 @@ describe('Dog model', () => {
   describe('Validators', () => {
     beforeEach(() => Dog.sync({ force: true }));
     describe('name', () => {
-      it('should throw an error if name is null', (done) => {
-        Dog.create({})
-          .then(() => done(new Error('It requires a valid name')))
-          .catch(() => done());
+      it('should throw an error if name is null', async () => {
+        try {
+          await Dog.create({heightMin: 4, heightMax: 5, weightMin: 1, weightMax: 5, image: "https://cdn.wallpapersafari.com/0/88/ujTDLZ.jpg"})
+          done()
+        } catch (error) {
+          expect(error.message).to.equal("notNull Violation: dog.name cannot be null");
+        }
       });
-      it('should work when its a valid name', () => {
-        Dog.create({ name: 'Pug' });
+      it('should work when its a valid name', async () => {
+        const dogCreated = await Dog.create({ name: "Pug", weightMin: 1, weightMax: 4, heightMin: 1, heightMax: 4, image: "https://cdn.wallpapersafari.com/0/88/ujTDLZ.jpg"})
+
+        const found = await Dog.findOne({
+          where: {
+            name: "Pug"
+          },
+        });
+
+        expect(await dogCreated.name).to.equal(await found.dataValues.name);
       });
     });
   });
--- api/tests/routes/dog.spec.js
+++ api/tests/routes/dog.spec.js
@@ -6,19 +6,59 @@ const { Dog, conn } = require('../../src/db.js');
 
 const agent = session(app);
 const dog = {
+  id: 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11',
   name: 'Pug',
+  heightMin: 1,
+  heightMax: 5,
+  weightMin: 1,
+  weightMax: 5,
+  image: "https://cdn.wallpapersafari.com/0/88/ujTDLZ.jpg",
+  temperament: "Active",
 };
 
-describe('Videogame routes', () => {
+const dogO = {
+  name: "Rodhesian",
+  heightMin: 2,
+  heightMax: 6,
+  weightMin: 2,
+  weightMax: 6,
+  image: "https://cdn.wallpapersafari.com/0/88/ujTDLZ.jpg",
+  temperament: "Stubborn",
+};
+
+const dogIncomp = {
+  heightMin: 3,
+  heightMax: 4,
+  weightMin: 3,
+  weightMax: 4,
+  temperament: ["obedient"],
+};
+
+describe('Dog routes', () => {
   before(() => conn.authenticate()
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   }));
   beforeEach(() => Dog.sync({ force: true })
     .then(() => Dog.create(dog)));
-  describe('GET /dogs', () => {
-    it('should get 200', () =>
-      agent.get('/dogs').expect(200)
+  describe('GET /dogs/:id', () => {
+    it('should return a dog', () =>{
+      agent.get('/dogs/a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11').expect(200).timeout(10000)
+    });
+  });
+  describe("GET /temperaments", () => {
+    it("should get 200 when get all temperaments", () =>
+      agent.get("/temperaments").expect(200)
+    );
+  });
+  describe("POST /dogs", () => {
+    it("should get 200 status when created", () =>
+      agent.post("/dogs")
+      .send(dogO).expect(200).timeout(10000)
+    );
+    it("should get 400 status when created", () =>
+      agent.post("/dogs")
+      .send(dogIncomp).expect(400).timeout(10000)
     );
   });
 });
